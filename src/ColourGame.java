f58446f45c973f98e9aa3f669742fbbff8e53f28
thunderjaw123 <58790357+thunderjaw123@users.noreply.github.com>
2021-01-25 19:15:14 -0500

    background music
    yeppers

--- src/ColourGame.java
+++ src/ColourGame.java
@@ -3,17 +3,24 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.Gdx;
 
 public class ColourGame extends Game { //implements ApplicationListener, ContactListener{
 	SpriteBatch batch;
   ShapeRenderer shapeRenderer;
-  BitmapFont font;
+	BitmapFont font;
+	Music background;
 
 	public void create () {
 		batch = new SpriteBatch();
 		shapeRenderer = new ShapeRenderer();
 		font = new BitmapFont();
 		setScreen(new TitleScreen(this));
+		background = Gdx.audio.newMusic(Gdx.files.internal("assets/audio/background.mp3"));
+		background.setVolume(0.2f);
+		background.setLooping(true);
+		background.play();
 	}
 
 	public void dispose () {
